627d1997f3e64d08227e6693489f3bef08a488d8
Gabriel <gabgdemarchi@gmail.com>
2022-05-21 14:26:00 -0300

    Alterações feitas na aula introdutória a OO da Alura

--- conta.py
+++ conta.py
@@ -3,16 +3,51 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print('Criando objeto na memória...{}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print('O titular {} tem um saldo de {}'.format(self.titular, self.saldo))
+        print('O titular {} tem um saldo de {}'.format(self.__titular, self.__saldo))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
     def sacar(self, valor):
-        self.saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print('O valor {} passou do limite permitido'.format(valor))
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'NU': '001', 'ITI': '023', 'C6': '092'}
